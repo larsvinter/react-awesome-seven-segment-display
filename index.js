63a9455f3540848b13187ed62776cb4ffde93d83
Lars Vinter <larsvinter@gmail.com>
2016-10-17 19:12:44 +0200

    Added initial files

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const AwesomeButton = require('./src/AwesomeButton.js')
+const AwesomeSevenSegmentDisplay = require('./src/AwesomeSevenSegmentDisplay.js')
 
-module.exports = AwesomeButton
+module.exports = AwesomeSevenSegmentDisplay
